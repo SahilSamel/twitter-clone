06251d47008338165362489817583161e3731118
Aaryan-Thakur <aaryan3120@gmail.com>
2023-06-07 14:52:50 +0530

    configuring firebase

---
+++ back-end/connections/firebase.js
@@ -0,0 +1,69 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  getRedirectResult,
+} from "firebase/auth";
+
+const createUser = () => {
+  const auth = getAuth();
+
+  createUserWithEmailAndPassword(auth, "aaryan3120@gmail.com", "123456789")
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+};
+
+const signIn = () => {
+  const auth = getAuth();
+
+  signInWithEmailAndPassword(auth, "aaryan3120@gmail.com", "123456789")
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+};
+
+const google = () => {
+  const provider = new GoogleAuthProvider();
+
+  console.log(provider)
+
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+
+export { createUser, signIn, google };
--- back-end/server.js
+++ back-end/server.js
@@ -4,12 +4,19 @@ import mongoose from "mongoose";
 import  application  from "express";
 import mongo from "./connections/mongoDB.js"
 import driver from "./connections/neo4j.js";
+import fapp from "./firebaseconfig.js";
+
+import {createUser,signIn,google} from "./connections/firebase.js";
 
 const app = express();
 
 //MongoDB Connection
 mongo();
 
+
+// createUser();
+// signIn();
+google();
 
 
 //Connection to port
