69894de79b3684171ebc93687ee4a290a60e6fa1
Aaryan-Thakur <aaryan3120@gmail.com>
2023-06-07 12:35:23 +0530

    mongoDB connection initial commit

--- back-end/connections/mongoDB.js
+++ back-end/connections/mongoDB.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import 'dotenv/config';
 
 const mongo = async () => {
   try {
-    mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.MongoDB);
+      mongoose.set('strictQuery', false);
+      mongoose.connect(process.env.MongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+
+    });
     console.log("Connected to MongoDB Atlas");
   } catch (error) {
     console.log(error);
