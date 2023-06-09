540f717127dae2f705ab4f1ec40ba3ab694faf8c
Aaryan-Thakur <aaryan3120@gmail.com>
2023-06-09 23:08:44 +0530

    SMTP testing

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -8,6 +8,8 @@ import {
 
 import verifyToken from "../middleware/verifyToken.js";
 
+// import nodemailer from "nodemailer"
+
 const auth = getAuth();
 const router = express.Router();
 
@@ -19,5 +21,35 @@ router.post("/signup", (req, res) => {
 router.post("/signin", (req, res) => {
   signIn(req, res);
 });
+
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.email,
+//     pass: process.env.password,
+//   },
+// });
+
+// const mailOptions = {
+//   from: process.env.email,
+//   to: 'aaryan3120@gmail.com',
+//   subject: 'Test Email',
+//   text: 'This is a test email sent using Nodemailer and Gmail SMTP.',
+// };
+
+// router.post("/email", (req, res) => {
+
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log('Error sending email:', error);
+//       res.status(201).json({"E":error})
+
+//     } else {  
+//       console.log('Email sent:', info.response);
+//     }
+//   });
+
+//   });
 
 export default router;
