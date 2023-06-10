ca6e4f72655b22acfb1c4cd5e234e3fdb86cbb4f
Aaryan-Thakur <aaryan3120@gmail.com>
2023-06-11 00:02:03 +0530

    Addded Delete Tweet Function

--- back-end/controller/tweetController.js
+++ back-end/controller/tweetController.js
@@ -9,45 +9,62 @@ const createTweet = (req, res) => {
   const timestamp = new Date();
   const { type, text, mediaURL, derivedUserId, derivedTweetId, threadId } =
     req.body;
-    
+
   const newThread = new Thread({ replies: [] });
 
-  newThread.save()
-  .then((savedThread) => {
-    const threadId = savedThread._id;
+  newThread
+    .save()
+    .then((savedThread) => {
+      const threadId = savedThread._id;
 
-    const newTweet = {
-      type: parseInt(type),
-      text: text || "",
-      mediaURL: mediaURL || "",
-      derivedUserId: derivedUserId || null,
-      derivedTweetId: derivedTweetId || null,
-      threadId: threadId || null,
-      timestamp: new Date(),
-      likes: [],
-    };
+      const newTweet = {
+        type: parseInt(type),
+        text: text || "",
+        mediaURL: mediaURL || "",
+        derivedUserId: derivedUserId || null,
+        derivedTweetId: derivedTweetId || null,
+        threadId: threadId || null,
+        timestamp: new Date(),
+        likes: [],
+      };
 
-    Tweet.findOneAndUpdate(
-      { userId },
-      { $push: { tweets: newTweet } },
-      { new: true, upsert: true }
-    )
-      .then((updatedUser) => {
-        console.log("Updated user:", updatedUser);
-      })
-      .catch((error) => {
-        console.error("Error updating user:", error);
-      });
-      
-  })
-  .catch((error) => {
-    console.error('Error creating thread:', error);
-  });
-
-
-
+      Tweet.findOneAndUpdate(
+        { userId },
+        { $push: { tweets: newTweet } },
+        { new: true, upsert: true }
+      )
+        .then((updatedUser) => {
+          console.log("Updated user:", updatedUser);
+        })
+        .catch((error) => {
+          console.error("Error updating user:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error creating thread:", error);
+    });
 };
 
+// Delete Tweet
+const deleteTweet = (req, res) => {
+  const { userId, objId } = req.body;
+
+  Tweet.updateOne(
+    { userId: userId, "tweets._id": objId },
+    { $pull: { tweets: { _id: objId } } },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        if (result.modifiedCount > 0) {
+          res.status(201).json({ "Message": "Tweet Deleted Successfully" });
+        } else {
+          res.status(201).json({ "Message": "No tweet found" });
+        }
+      }
+    }
+  );
+};
 //  <--- End of TWEET FUNCTIONS --->
 
-export { createTweet };
+export { createTweet, deleteTweet };
--- back-end/routes/tweetRoutes.js
+++ back-end/routes/tweetRoutes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
-import { createTweet } from "../controller/tweetController.js";
+import { createTweet, deleteTweet } from "../controller/tweetController.js";
 const router = express.Router();
 
 //<--- TwEET FUNCTIONS --->
 
-
 router.post("/tweet", verifyToken, (req, res) => {
-    createTweet(req, res);
-  });
+  createTweet(req, res);
+});
 
+router.post("/delete", verifyToken, (req, res) => {
+  deleteTweet(req, res);
+});
 
 //<--- End of TwEET FUNCTIONS --->
-
 
 export default router;
