194b89ab8f21342ddfbe060d76788ac57547bf2e
Sahil Samel <sahilsamel134@gmail.com>
2023-06-21 11:11:43 +0530

    Sign Up component styling done

--- front-end/components/SignUp.tsx
+++ front-end/components/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useForm, SubmitHandler } from "react-hook-form";
-import authpost from "@/api/authpost"
+import authpost from "@/api/authpost";
 
 type Inputs = {
   email: string;
@@ -11,38 +11,99 @@ export default function SignUp() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
-  
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     const jsonData = JSON.stringify(data);
     console.log(data);
-    authpost(
-        '/auth/signup',
-        jsonData,
-        function(err: any,data: any){
-            if(err){
-                console.log(err);
-            }else{
-                console.log(jsonData);
-            }
-        }
-    )
+    authpost("/auth/signup", jsonData, function (err: any, data: any) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(jsonData);
+      }
+    });
   };
 
   return (
-    <form className="flex justify-center flex-col" onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="Enter Email" className="my-5" {...register("email", { required: true })} />
-      {errors.email && <span>This field is required</span>}
-
-      <input placeholder="Enter Password" className="my-5" {...register("password", { required: true })} />
-      {errors.password && <span>This field is required</span>}
-
-      <input placeholder="Enter User Handle" className="my-5" {...register("userHandle", { required: true })} />
-      {errors.userHandle && <span>This field is required</span>}
-
-      <input className="text-white" type="submit" />
-    </form>
+    <div className="flex justify-center items-center h-screen bg-blue-200">
+      <div className="bg-white rounded-lg shadow-lg p-6">
+        <h1 className="text-3xl font-bold mb-6 text-black text-center">
+          Sign up for Twitter
+        </h1>
+        <form
+          className="w-full max-w-sm mx-auto flex flex-col"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-xs">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <span className="text-red-500 text-xs">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="userHandle"
+            >
+              User Handle
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="User Handle"
+              {...register("userHandle", { required: true })}
+            />
+            {errors.userHandle && (
+              <span className="text-red-500 text-xs">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
+
--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
 import Login from "@/components/Login";
 import Home from "@/pages/home";
+import SignUp from "@/components/SignUp";
 
 export default function Index() {
   const token = useSelector((state: any) => state.auth.token);
 
   // Check if user is logged in
   if (!token) {
-    return <Login />;
+    return <SignUp />;
   }
 
   // User is logged in, render main content
