8abd5cf8964386602f0eaceec3a656f10f03a49c
Sahil Samel <sahilsamel134@gmail.com>
2023-07-14 15:02:54 +0530

    UI updates

--- front-end/components/Sidebar.tsx
+++ front-end/components/Sidebar.tsx
@@ -12,11 +12,9 @@ import {
   AiOutlineSetting,
   AiOutlineLogout,
 } from "react-icons/ai";
-import { useWindowSize } from "@/utils/Windowsize"; // Custom hook for getting window size
+
 
 const Sidebar = () => {
-  const { width } = useWindowSize(); // Get window width using custom hook
-  const isMobile = width <= 1222; // Define breakpoint for mobile screens
   const dispatch = useDispatch();
   const router = useRouter();
   const userHandle = useSelector((state: any) => state.auth.userHandle);
@@ -36,73 +34,79 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="fixed flex flex-col gap-5 justify-start items-start max-w-xs ">
+    <div className="flex flex-col gap-5 justify-start items-start max-w-max px-2 text-2xl text-regular mobile:fixed mobile:flex-row mobile:bottom-0 mobile:left-0 mobile:right-0">
+      <div className="py-2 px-4 mobile:hidden">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/twitter-clone-ratio.appspot.com/o/icons8-twitter-an-american-online-news-and-social-networking-service-24.png?alt=media&token=e94558c2-116d-4cf6-bba4-224ece1a22e0"
+          alt="twitter logo white"
+        />
+      </div>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:text-2xl mobile:text-xl hover:bg-blue-100 hover:text-blue-500`}
         onClick={() => {
           router.push("/home");
         }}
       >
         <AiOutlineHome />
-        {!isMobile && <span>Home</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Home</span>
+        </div>
       </button>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:text-2xl mobile:text-xl hover:bg-blue-100 hover:text-blue-500`}
         onClick={handleProfileClick}
       >
         <AiOutlineUser />
-        {!isMobile && <span>Profile</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Profile</span>
+        </div>
       </button>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:hidden hover:bg-blue-100 hover:text-blue-500`}
       >
         <AiOutlineBell />
-        {!isMobile && <span>Notifications</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Notifications</span>
+        </div>
       </button>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:text-2xl mobile:text-xl hover:bg-blue-100 hover:text-blue-500`}
         onClick={() => {
           router.push("/bookmarks");
         }}
       >
         <AiOutlineBook />
-        {!isMobile && <span>Bookmark</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Bookmarks</span>
+        </div>
       </button>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:hidden hover:bg-blue-100 hover:text-blue-500`}
       >
         <AiOutlineForm />
-        {!isMobile && <span>Tweet</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Tweet</span>
+        </div>
       </button>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:text-2xl mobile:text-xl hover:bg-blue-100 hover:text-blue-500`}
         onClick={() => {
           router.push("/settings");
         }}
       >
         <AiOutlineSetting />
-        {!isMobile && <span>Settings</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Settings</span>
+        </div>
       </button>
       <button
-        className={`flex items-center gap-2 px-4 py-2 rounded-lg ${
-          isMobile ? "text-3xl" : "text-2xl"
-        } hover:bg-blue-100 hover:text-blue-500`}
+        className={`flex items-center gap-2 px-4 py-2 rounded-lg mobile:hidden hover:bg-blue-100 hover:text-blue-500`}
         onClick={handleLogout}
       >
         <AiOutlineLogout />
-        {!isMobile && <span>Account (Logout)</span>}
+        <div className="mobile:hidden tablet:hidden">
+          <span>Logout</span>
+        </div>
       </button>
     </div>
   );
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'mobile': {'max':'425px'},
+
+      'tablet': {'max':'768px'},
+
+      'laptop': {'max':'1024px'},
+
+      'desktop': {'max':'1280px'},
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -14,6 +23,9 @@ module.exports = {
       },
       textUnderlineOffset: {
         18: '18px',
+      },
+      dropShadow: {
+        'standard':'0 0 0.35rem #CCCCCC'
       }
     },
   },
